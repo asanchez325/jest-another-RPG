69d0bca61e8973b605da814dec4917c987dab3dc
Imperial Design <amsanchez@knox.edu>
2020-11-23 11:14:46 -0600

    complete player object

--- _tests_/Potion.test.js
+++ _tests_/Potion.test.js
@@ -1,4 +1,5 @@
 const Potion = require('../lib/Potion.js');
+const Player = require('../lib/Player');
 
 test('creates a health potion object', () => {
     const potion = new Potion('health');
@@ -13,4 +14,23 @@ test('creates a health potion object', () => {
     expect(potion.name).toEqual(expect.any(String));
     expect(potion.name.length).toBeGreaterThan(0);
     expect(potion.value).toEqual(expect.any(Number));
+  });
+
+  test("gets player's stats as an object", () => {
+    const player = new Player('Dave');
+  
+    expect(player.getStats()).toHaveProperty('potions');
+    expect(player.getStats()).toHaveProperty('health');
+    expect(player.getStats()).toHaveProperty('strength');
+    expect(player.getStats()).toHaveProperty('agility');
+  });
+
+  test('gets inventory from player or returns false', () => {
+    const player = new Player('Dave');
+  
+    expect(player.getInventory()).toEqual(expect.any(Array));
+  
+    player.inventory = [];
+  
+    expect(player.getInventory()).toEqual(false);
   });
