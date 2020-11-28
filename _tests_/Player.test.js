7a6905e3ace2851ed62817fbe7cb2aa758ed015b
Imperial Design <amsanchez@knox.edu>
2020-11-28 02:08:02 -0600

    add character and change to ES6

--- _tests_/Player.test.js
+++ _tests_/Player.test.js
@@ -12,7 +12,27 @@ test('creates a player object', () => {
     expect(player.agility).toEqual(expect.any(Number));
     expect(player.inventory).toEqual(
     expect.arrayContaining([expect.any(Object)])
-  );})
+  );
+});
+
+test("gets player's stats as an object", () => {
+  const player = new Player('Terahje the Chosen');
+
+  expect(player.getStats()).toHaveProperty('potions');
+  expect(player.getStats()).toHaveProperty('health');
+  expect(player.getStats()).toHaveProperty('strength');
+  expect(player.getStats()).toHaveProperty('agility');
+});
+
+test('gets inventory from player or returns false', () => {
+  const player = new Player('Terahje the Chosen');
+
+  expect(player.getInventory()).toEqual(expect.any(Array));
+
+  player.inventory = [];
+
+  expect(player.getInventory()).toEqual(false);
+});
 
   test("gets player's health value", () => {
     const player = new Player('Dave');
--- _tests_/Potion.test.js
+++ _tests_/Potion.test.js
@@ -2,35 +2,17 @@ const Potion = require('../lib/Potion.js');
 const Player = require('../lib/Player');
 
 test('creates a health potion object', () => {
-    const potion = new Potion('health');
-  
-    expect(potion.name).toBe('health');
-    expect(potion.value).toEqual(expect.any(Number));
-  });
+  const potion = new Potion();
 
-  test('creates a random potion object', () => {
+  expect(potion.name).toEqual(expect.any(String));
+  expect(potion.name.length).toBeGreaterThan(0);
+  expect(potion.value).toEqual(expect.any(Number));
+});
+
+test('creates a random potion object', () => {
     const potion = new Potion();
-  
+
     expect(potion.name).toEqual(expect.any(String));
     expect(potion.name.length).toBeGreaterThan(0);
     expect(potion.value).toEqual(expect.any(Number));
-  });
-
-  test("gets player's stats as an object", () => {
-    const player = new Player('Dave');
-  
-    expect(player.getStats()).toHaveProperty('potions');
-    expect(player.getStats()).toHaveProperty('health');
-    expect(player.getStats()).toHaveProperty('strength');
-    expect(player.getStats()).toHaveProperty('agility');
-  });
-
-  test('gets inventory from player or returns false', () => {
-    const player = new Player('Dave');
-  
-    expect(player.getInventory()).toEqual(expect.any(Array));
-  
-    player.inventory = [];
-  
-    expect(player.getInventory()).toEqual(false);
-  });
+});
